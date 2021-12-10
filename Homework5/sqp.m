ddc34cd842b538794c0eaaeed2a99a4b438164cc
Omik Save <omiksave@gmail.com>
2021-12-10 08:31:51 -0700

    Updating sqp

--- Homework5/sqp.m
+++ Homework5/sqp.m
@@ -2,4 +2,8 @@ function sqp(x0,eps)
 %Setting up initial x
 x = x0;
 %Initializing Hessian
-W = eye(2) % 2 Variables
+W = eye(2); % 2 Variables
+%Initialing mu
+mu_old = zeros(2,1);
+%Intializing weights
+w = zeros(2,1);
